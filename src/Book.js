418c4ab01fd1ca0d7685b08c0bdeb7168aa2a895
Sayantani Chaudhuri <sayantani.chaudhuri@gmail.com>
2021-07-02 21:12:54 +1000

    bug fix: fixed multiple author name display issue

--- src/Book.js
+++ src/Book.js
@@ -45,7 +45,7 @@ class Book extends Component {
                     </div>
                 </div>
                 <div className="book-title">{book.title}</div>
-                <div className="book-authors">{book.authors}</div>
+                <div className="book-authors">{book.authors && book.authors.join(', ')}</div>
             </div>
         );
     }
